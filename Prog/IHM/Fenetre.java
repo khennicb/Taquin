e12189573f78f5334f82ee37e2f1356d93935caa
NonoDesBois <noon.38@free.fr>
2016-02-19 12:26:30 +0100

    Ajout de "MainNono"

--- Prog/IHM/Fenetre.java
+++ Prog/IHM/Fenetre.java
@@ -6,6 +6,9 @@
 package Prog.IHM;
 
 import Prog.EtatPlateau;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import javax.swing.JFrame;
 
 /**
@@ -13,6 +16,20 @@ import javax.swing.JFrame;
  * @author foreyn
  */
 public class Fenetre extends JFrame implements IHM{
+    
+    public Fenetre(){
+        super("Taquin");
+        
+        WindowListener l = new WindowAdapter() {
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        };
+        
+        addWindowListener(l);
+        setSize(200, 200);
+        setVisible(true);
+    }
 
     @Override
     public void afficherEtat(EtatPlateau etat) {
