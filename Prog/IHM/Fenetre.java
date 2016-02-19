8a49c0aa141c1f4c3c4dff8955f1cedd406e7db6
NonoDesBois <noon.38@free.fr>
2016-02-19 12:20:29 +0100

    Ajout de la class "Fenetre"

---
+++ Prog/IHM/Fenetre.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prog.IHM;
+
+import Prog.EtatPlateau;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author foreyn
+ */
+public class Fenetre extends JFrame implements IHM{
+
+    @Override
+    public void afficherEtat(EtatPlateau etat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
