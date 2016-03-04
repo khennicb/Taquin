c7ffc71682701087f712e3fd183a28ba356c6799
NonoDesBois <noon.38@free.fr>
2016-03-04 10:27:38 +0100

    Ajoute de règles du jeu

--- Prog/Jeu.java
+++ Prog/Jeu.java
@@ -25,13 +25,7 @@ public class Jeu {
     
     public void lancerLeJeu(){
         out.getExplications(etat);
-        /*
-        out.afficheMessage("Etat Initial : ");
-        out.afficherEtat(etat);
         
-        
-        out.afficheEtatFinal(etat);
-        */
         Solveur s = new Solveur(etat);
         if (!s.estSolvable()) {
             out.afficheMessage("Cette grille n'est pas solvable.");
