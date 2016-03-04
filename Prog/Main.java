131ca04582814d32ccd1532a81587d4e4ee72c64
NonoDesBois <noon.38@free.fr>
2016-03-04 12:40:01 +0100

    Merge origin/master
    Conflicts:
    	Prog/Main.java

--- Prog/Main.java
+++ Prog/Main.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     
@@ -120,7 +122,11 @@ public class Main {
         if (solveur.estSolvable()) {
             if ("Jeux".equals(mode)){
                 Jeu jeu = new Jeu(init);
-                jeu.lancerLeJeu();
+                try {
+                    jeu.lancerLeJeu();
+                } catch (ExceptionQuitter ex) {
+                    
+                }
             } else if ("A*".equals(mode)){
                 SolveurSniper s = new SolveurSniper(init);
                 EtatPlateau sol = s.solve();
