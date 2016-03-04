4c2c02f60dc6d214dd314f8c620e778f1d9cc45a
NonoDesBois <noon.38@free.fr>
2016-03-04 10:02:41 +0100

    Amélioration Jeu

--- Prog/Jeu.java
+++ Prog/Jeu.java
@@ -48,13 +48,13 @@ public class Jeu {
             boolean depPossible;
             
             do {                
-                int d = out.listen();
+                String d = out.listen();
                 
-                if (d==8) {
+                if (d.equalsIgnoreCase("8")) {
                     deplacement = Deplacement.Haut;
-                } else if (d==2) {
+                } else if (d.equalsIgnoreCase("2")) {
                     deplacement = Deplacement.Bas;
-                } else if (d==4) {
+                } else if (d.equalsIgnoreCase("4")) {
                     deplacement = Deplacement.Gauche;
                 } else {
                     deplacement = Deplacement.Droite;
