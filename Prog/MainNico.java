96a3f2c4000383d81f3831e809c3352773ef50ce
Colisan Loveteit <nilco@laposte.net>
2016-03-04 09:03:45 +0100

    Decomposition f = g+h

--- Prog/MainNico.java
+++ Prog/MainNico.java
@@ -19,12 +19,26 @@ public class MainNico {
         IHM.afficherEtat(test);
         */
         
-        int[][] tab = {
-            {1, 2},
-            {3, 4}
+        int[][] debut = {
+            {4, 8, 7},
+            {6, 2, 3},
+            {1, 0, 5}
         };
-        //EtatPlateau test = new EtatPlateau("HBBHGD", tab);
         
+        int[][] fin = {
+            {1, 2, 3},
+            {3, 4, 5},
+            {6, 7, 0}
+        };
+        
+        EtatPlateau init = new EtatPlateau("", debut, fin);
+        EtatPlateau end = new EtatPlateau("", fin, fin);
+        
+        IHM IHM = new Console();
+        
+        SolveurSniper r = new SolveurSniper(IHM);
+        
+        r.solve(init, end);
         
     }
     
--- Prog/SolveurSniper.java
+++ Prog/SolveurSniper.java
@@ -1,9 +1,15 @@
 package Prog;
 
+import Prog.IHM.IHM;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class SolveurProfondeur {
+public class SolveurSniper {
+    private final IHM sortie;
+
+    public SolveurSniper(IHM sortie) {
+        this.sortie = sortie;
+    }
     
     public EtatPlateau solve(EtatPlateau init, EtatPlateau fin){
         //Stack<EtatPlateau> listeFermee = new Stack<>();
@@ -15,13 +21,16 @@ public class SolveurProfondeur {
         
         while(!listeOuverte.isEmpty()){
             EtatPlateau current = listeOuverte.poll();
+            
+            if(sortie!=null) sortie.afficherEtat(current);
+            System.out.println("f() = g() + h() = "+current.getG()+" + "+current.getH()+" = "+current.getF());
             if(current.toHashKey().equals(fin.toHashKey())){
                 return current;
             }else{
                 for(Deplacement d : Deplacement.values()){
                     EtatPlateau suivant = current.getEtatPlateauApresAction(d);
                     if(suivant != null){
-                       listeOuverte.add(suivant);
+                       listeOuverte.offer(suivant);
                    }
                }
            }
@@ -34,15 +43,15 @@ class ManhattanComparator implements Comparator<EtatPlateau> {
     
     @Override
     public int compare(EtatPlateau etat1, EtatPlateau etat2) {
-        int manE1 = etat1.getScoreManatthan();
-        int manE2 = etat2.getScoreManatthan();
+        int manE1 = etat1.getH();
+        int manE2 = etat2.getH();
         
         if(manE1 < manE2){
-            return -1;
+            return 1;
         }else if(manE1 == manE2){
             return 0; // ou retourner seulement le g de f=g+h
         }else{
-            return 1;
+            return -1;
         }
     }
     
